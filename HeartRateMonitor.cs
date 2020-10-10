95bac3596403cb3918b2c0e46158c927e6fa0490
Zachary Emerson <zacisme07@gmail.com>
2020-10-10 19:58:46 -0400

    Add files via upload
    HeartRateMonitor is main program and HeartRates is the class file used.

---
+++ HeartRateMonitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HeartRateMonitor
+{
+    class HeartRateMonitor
+    {
+        static void Main(string[] args)
+        {
+            //Heart Rate Monitor App
+            //Author Zachery Emerson
+
+            string fName;
+            string lName;
+            int birthYear;
+            int currYear;
+
+            //Intorduction 
+            Console.WriteLine("Want to know more about your heart?");
+            Console.WriteLine("We just need to gather some information first...");
+            
+            //Gather information
+            Console.Write("First, Please enter your first name: ");
+            fName = Console.ReadLine();
+            Console.Write("Next, Please tell me your last name: ");
+            lName = Console.ReadLine();
+            Console.Write("And what year were you born in: ");
+            birthYear = int.Parse(Console.ReadLine());
+            Console.Write("Finally, What year is it currently: ");
+            currYear = int.Parse(Console.ReadLine());
+
+            HeartRates heartrate1 = new HeartRates(fName, lName, birthYear, currYear);
+
+            Console.WriteLine($"Your name is {heartrate1.FName} {heartrate1.LName} and you were born in {heartrate1.BirthYear}");
+            Console.WriteLine($"Your age is {heartrate1.Age(heartrate1.BirthYear, heartrate1.CurrentYear)}");
+            Console.WriteLine($"Your Maximum Heart Rate is {heartrate1.MaxHeartRate()}");
+            Console.WriteLine($"Your target heart range is {heartrate1.MinTarget()} - {heartrate1.MaxTarget()}");
+        }
+    }
+}
---
+++ HeartRates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeartRateMonitor
+{
+    class HeartRates
+    {
+        public string FName { get; set; }
+        
+        public string LName { get; set; }
+        
+        public int BirthYear { get; set; }
+
+        public int CurrentYear { get; set; }
+
+        public HeartRates(string fName, string lName, int birthYear, int currYear)
+        {
+            FName = fName;
+            LName = lName;
+            BirthYear = birthYear;
+            CurrentYear = currYear;
+        }
+
+        public int Age(int birthYear, int currYear)
+        {
+            int age = currYear - birthYear;
+            
+            return age;
+        }
+
+        public int MaxHeartRate()
+        {
+            int maxHeartRate = 220 - Age(BirthYear, CurrentYear);
+
+            return maxHeartRate;
+        }
+
+        public double MinTarget()
+        {
+            double minTarget = MaxHeartRate() * 0.5;
+
+            return minTarget;
+        }
+
+        public double MaxTarget()
+        {
+            double maxTarget = MaxHeartRate() * 0.85;
+
+            return maxTarget;
+        }
+    }
+}
